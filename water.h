92943d9152515180259998b3c8370df685e41ad4
SophieAu <SophieAu@users.noreply.github.com>
2016-04-14 12:13:33 +1000

    water.h

---
+++ water.h
@@ -0,0 +1,9 @@
+extern void resetSegments(int globalSegments);
+extern void waterInit(int globalSegments);
+
+extern void toggleWireFrame();
+extern void toggleNormal();
+extern void toggleTangent();
+
+extern float getSineY(float x);
+extern void drawWater(float globalTime)
